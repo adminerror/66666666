843735e68ce5669a0a71a42e88fab158d03e28b2
adminerror <mohamad.smb.m@gmail.com>
2015-12-28 20:10:42 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -222,7 +222,7 @@ function create_config( )
     },
     sudo_users = {37125955,0,tonumber(our_id) },--Sudo users
     disabled_channels = {},
-    realm = {},--Realms Id
+    realm = {89481501},--Realms Id
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v1
 An advance Administration bot based on yagop/telegram-bot 
