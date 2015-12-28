2c319a43c17def242c4535d82d0bcadf45fffea5
adminerror <mohamad.smb.m@gmail.com>
2015-12-28 20:05:27 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {37125955(our_id)},--Sudo users
+    sudo_users = {37125955,0,tonumber(our_id) },--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
